283337ce976c604a0ef8c394346a9255df714f0f
Aaron Alphons Thomas <aaronalphons14@gmail.com>
2026-04-16 14:35:56 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,1 +1,31 @@
-
+package com.library;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class AppTest {
+    @Test
+    public void testNoFine() {
+        double fine = App.calculateFine(10);
+        assertEquals(0.0, fine, 0.001);
+    }
+    @Test
+    public void testNoFineAtLimit() {
+        double fine = App.calculateFine(14);
+        assertEquals(0.0, fine, 0.001);
+    }
+    @Test
+    public void testFineOneExtraDay() {
+        double fine = App.calculateFine(15);
+        assertEquals(2.0, fine, 0.001);
+    }
+    @Test
+    public void testFineFiveExtraDays() {
+        double fine = App.calculateFine(19);
+        assertEquals(10.0, fine, 0.001);
+    }
+    @Test
+    public void testStatusOnTime() {
+        String status = App.getBorrowingStatus(7);
+        assertEquals("ON TIME", status);
+    }
+  
+}
